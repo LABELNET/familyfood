6fa263e99a2eabf48a67c4424d5fe484c3438f11
原明卓 <1406046087@qq.com>
2016-05-21 00:24:25 +0800

    【EDIT】 添加用户登录页面

---
+++ FamilyFood/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FamilyFood.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 用户登录页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Login() {
+            return View();
+        }
+
+    }
+}
