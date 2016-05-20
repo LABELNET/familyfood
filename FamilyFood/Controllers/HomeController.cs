19aded216a2e7d9b1283def1a44d7e07a6d88b0b
原明卓 <1406046087@qq.com>
2016-05-20 17:46:46 +0800

    【ADD】添加index主页

---
+++ FamilyFood/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FamilyFood.Controllers
+{
+    public class HomeController : Controller
+
+    {
+
+       private FamilyFood.Models.FamilyFoodEntities db = new Models.FamilyFoodEntities();
+
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+    }
+}
---
+++ FamilyFood/Views/Home/index.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Shared/index.Master" Inherits="System.Web.Mvc.ViewPage<dynamic>" %>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="head" runat="server">
+  <title>主页</title>
+</asp:Content>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+   <h2>我是主页</h2>
+
+</asp:Content>
