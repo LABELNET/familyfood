fc4f6a1b211941fbecdf65f0e60c4cdb873dd84e
原明卓 <1406046087@qq.com>
2016-05-21 16:25:16 +0800

    【EDIT】实现登录业务

--- FamilyFood/Controllers/UserController.cs
+++ FamilyFood/Controllers/UserController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FamilyFood.Models;
 
 namespace FamilyFood.Controllers
 {
     public class UserController : Controller
     {
+
+
+        private FamilyFood.Models.FamilyFoodEntities db = new Models.FamilyFoodEntities();
+
         //
         // GET: /User/
 
@@ -33,6 +38,26 @@ namespace FamilyFood.Controllers
         {
             return View();
         }
+
+        
+        /// <summary>
+        /// 用户登录请求
+        /// </summary>
+        /// <param name="phone">电话</param>
+        /// <param name="pass">密码</param>
+        /// <returns></returns>
+        public ActionResult LoginRequest(String phone, String pass) { 
+           user_table user= db.user_table.SingleOrDefault<user_table>(u=>u.phone==phone && u.pass==pass);
+           if (user.id > 0)
+           {
+               Session["user"] = user;
+               return Redirect("home/index");
+           }
+           else {
+               return Redirect("user/register");
+           }
+        }
+        
 
 
 
